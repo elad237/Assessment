df50ab57acc999b4979df2f4433535bc2299fe58
elad237 <tumbu.elad@gmail.com>
2023-01-03 13:19:13 +0100

    Updated module folder

--- __tests__/comments.test.js
+++ __tests__/comments.test.js
@@ -1,4 +1,5 @@
 import getComments from '../src/module/comments/comment.js';
+
 describe('Add tests for comments counter', () => {
   it('Three Comments in a movie - Output: 3', async () => {
     const commentsNumber = await getComments(53881); // Run the function
@@ -6,12 +7,12 @@ describe('Add tests for comments counter', () => {
   });
 
   it('No Comments in a movie - Output: Undefined', async () => {
-    const commentsNumber = await getComments(538851); // Run the function
-    expect(commentsNumber.length).toEqual(undefined); // Make an assertion on the result
+    const commentsNumber = await getComments(538851); // To Run the function
+    expect(commentsNumber.length).toEqual(undefined); // To Make an assertion on the result
   });
 
   it('One Comments in a movie - Output: 1', async () => {
-    const commentsNumber = await getComments(36306); // Run the function
-    expect(commentsNumber.length).toEqual(1); // Make an assertion on the result
+    const commentsNumber = await getComments(36306); // To Run the function
+    expect(commentsNumber.length).toEqual(1); // To Make an assertion on the result
   });
 });
